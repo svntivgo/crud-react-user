31f7123ef1100a76e2e9cf9869db779bb59dc0e2
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-22 03:25:18 -0500

    se crea AddUserForm addUser

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
 import React, { useState } from 'react';
 import UserTable from './components/UserTable';
 import { v4 as uuidv4 } from 'uuid';
-
-const usersData = [
-{ id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
-{ id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
-{ id: uuidv4(), name: 'Ben', username: 'benisphere' },
-]
+import AddUserForm from './components/AddUserForm';
 
 function App() {
+    const usersData = [
+        { id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+        { id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+        { id: uuidv4(), name: 'Ben', username: 'benisphere' },
+    ]
+
+    const [users, setUsers] = useState(usersData)
+
+    /**
+     * Crea un nuevo usuario
+     * @param {*} user
+     */
+    const addUser = (user) => {
+        user.id = uuidv4()
+        setUsers([...users, user])
+    }
+
     return (
         <div className="container">
             <h1>CRUD App with Hooks</h1>
             <div className="flex-row">
                 <div className="flex-large">
                 <h2>Add user</h2>
+                <AddUserForm addUser={addUser}/>
                 </div>
                 <div className="flex-large">
                 <h2>View users</h2>
-                <UserTable users={usersData}/>
+                <UserTable users={users}/>
                 </div>
             </div>
         </div>
