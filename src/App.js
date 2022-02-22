72fe7555e730da9a6b52b8a5c43d8b084ab70f1d
Santiago Barrera <santiago.barrerae@gmail.com>
2022-02-22 03:20:22 -0500

    se crea UserTable usersData

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import UserTable from './components/UserTable';
+import { v4 as uuidv4 } from 'uuid';
+
+const usersData = [
+{ id: uuidv4(), name: 'Tania', username: 'floppydiskette' },
+{ id: uuidv4(), name: 'Craig', username: 'siliconeidolon' },
+{ id: uuidv4(), name: 'Ben', username: 'benisphere' },
+]
 
 function App() {
-  return (
-    <div></div>
-  );
+    return (
+        <div className="container">
+            <h1>CRUD App with Hooks</h1>
+            <div className="flex-row">
+                <div className="flex-large">
+                <h2>Add user</h2>
+                </div>
+                <div className="flex-large">
+                <h2>View users</h2>
+                <UserTable users={usersData}/>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
